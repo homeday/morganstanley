7cdf7d6921c0812f9b04f04158687d08ed2b7777
Kim Guo <gyb_duckula@126.com>
2025-01-14 21:54:02 +0800

    test

--- newone.groovy
+++ newone.groovy
@@ -58,3 +58,57 @@ def call(String agentType, String imageName) {
     return yamlBuilder.toString()
 }
 
+
+
+
+pipeline {
+    agent any
+    stages {
+        stage('Example') {
+            steps {
+                script {
+                    // Define a password variable
+                    def pass = 'secretPassword'
+                    
+                    // Use the Mask Passwords Plugin to mask the password
+                    maskPasswords(varPasswordPairs: [[var: 'MY_PASSWORD', password: pass]]) {
+                        sh 'echo Password is: $MY_PASSWORD'
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+// Requires https://plugins.jenkins.io/mask-passwords to run
+
+/**
+ * Runs code with secret environment variables and hides the values.
+ *
+ * @param varAndPasswordList - A list of Maps with a 'var' and 'password' key.  Example: `[[var: 'TOKEN', password: 'sekret']]`
+ * @param Closure - The code to run in
+ * @return {void}
+ */
+def withSecretEnv(List<Map> varAndPasswordList, Closure closure) {
+  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: varAndPasswordList]) {
+    withEnv(varAndPasswordList.collect { "${it.var}=${it.password}" }) {
+      closure()
+    }
+  }
+}
+
+// Example code:
+node {
+  withSecretEnv([[var: 'VAULT_TOKEN', password: 'toosekret']]) {
+    sh '''#!/bin/bash -eu
+    echo "with env use:      ${VAULT_TOKEN}"
+    sleep 1
+    echo "without env use:   toosekret"
+    sleep 1
+    echo "just the var name: VAULT_TOKEN"
+    '''
+    sleep 1
+    echo "Outside SH: VAULT_TOKEN=${VAULT_TOKEN}"
+  }
+}
