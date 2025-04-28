98e94bb3a84b3beb4a66b8c6511b289f2485a051
Kim Guo <gyb_duckula@126.com>
2025-04-28 21:15:52 +0800

    a

--- agent/s1.sh
+++ agent/s1.sh
@@ -7,3 +7,8 @@ SECRET=$(vault read -field=secret secret/jenkins/agent)
 
 # Send the secret to the Unix domain socket
 echo -n "$SECRET" | socat - UNIX-CONNECT:/tmp/jenkins_socket/agent.sock
+
+
+Jenkins credentials can be securely managed within Jenkins itself, allowing them to be utilized when jobs run on any type of agent. These credentials are stored in an encrypted form on the Jenkins controller, ensuring secure access during job execution.​
+
+Alternatively, Vault can be employed by users, which is particularly advantageous when jobs run on Bring Your Own Agent (BYOA) setups. In such scenarios, users often have the flexibility to configure access to Vault using methods like Kerberos or mutual TLS (mTLS).
