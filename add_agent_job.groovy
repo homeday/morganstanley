79a69517dd446e369b4700844ac411055b690db9
homeday <gyb_duckula@126.com>
2025-11-13 20:38:40 +0800

    11

--- add_agent_job.groovy
+++ add_agent_job.groovy
@@ -28,3 +28,73 @@ node {
         }
     }
 }
+
+
+import com.cloudbees.hudson.plugins.folder.*
+import jenkins.model.*
+
+def folder = Jenkins.instance.getItemByFullName("folderA")
+def prop = folder.getProperties().get(Folder.ComputedFolderRunExclusiveProperty)
+
+println prop
+
+//=========================
+
+import com.cloudbees.hudson.plugins.folder.*
+import com.cloudbees.hudson.plugins.folder.properties.FolderJobProperty
+import jenkins.model.*
+
+def folder = Jenkins.instance.getItemByFullName("folderA")
+def prop = folder.getProperties().get(FolderJobProperty)
+if (prop != null && prop.getRestrictLabel() != null) {
+    println "Folder restricts jobs to label: ${prop.getRestrictLabel()}"
+} else {
+    println "No restriction set."
+}
+
+
+//===========================
+
+import com.cloudbees.hudson.plugins.folder.*
+import com.cloudbees.hudson.plugins.folder.properties.FolderJobProperty
+import jenkins.model.*
+
+def folder = Jenkins.instance.getItemByFullName("folderA")
+def prop = new FolderJobProperty(true, "k8s-a")  // (restrict=true, label='k8s-a')
+folder.addProperty(prop)
+folder.save()
+println "Restriction set for folderA to label 'k8s-a'"
+
+
+//===========================
+import com.cloudbees.hudson.plugins.folder.*
+import com.cloudbees.hudson.plugins.folder.properties.FolderJobProperty
+import jenkins.model.*
+
+// Change these variables
+def folderName = "folderA"
+def newLabel = "k8s-b"
+
+def folder = Jenkins.instance.getItemByFullName(folderName)
+if (folder == null) {
+    println "Folder '${folderName}' not found!"
+    return
+}
+
+// Check if the property already exists
+def prop = folder.getProperties().get(FolderJobProperty)
+
+if (prop != null) {
+    println "Existing restriction found: ${prop.getRestrictLabel()}"
+    prop.setRestrictLabel(newLabel)
+    prop.setRestrictRunWhere(true)
+    println "Updated restriction to '${newLabel}'"
+} else {
+    println "No restriction found, creating a new one..."
+    def newProp = new FolderJobProperty(true, newLabel)
+    folder.addProperty(newProp)
+}
+
+folder.save()
+println "Folder restriction is now set to '${newLabel}'"
+
